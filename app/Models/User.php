e24a2058b805acaf9ff22f31999484a5815eb2f9
Mehul Bawadia <mehulb.yourdevexpert@gmail.com>
2022-04-29 16:50:42 +0530

    Modified users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
+
+    /**
+     * The dates that will be mutated to Carbon instance.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'c_time', 'deleted_at',
+    ];
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +27,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'first_name', 'last_name', 'email', 'password', 'verification_token', 'status', 'c_time',
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 128);
             $table->rememberToken();
+            $table->enum('status', ['0', '1'])->nullable(); // 0 = Pending, 1 = Verifiied
+            $table->timestamp('c_time')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
